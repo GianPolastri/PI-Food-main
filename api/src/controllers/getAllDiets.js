d81a4a91e552c8004f2cf4b91932a1eb6bd20c93
Gian <gianpolastri@gmial.com>
2023-04-18 21:15:53 -0300

    Emprolijado de getAllDiets

--- api/src/controllers/getAllDiets.js
+++ api/src/controllers/getAllDiets.js
@@ -1,6 +1,4 @@
-const axios = require('axios');
-require('dotenv').config();
-const {DB_USER, DB_HOST, DB_PASSWORD} = process.env;
+
 const { Diet } = require('../db');
 
 
@@ -14,11 +12,10 @@ module.exports = async (req, res) => {
     try {
         // await dietsToDB();
         const diets = await Diet.findAll();
-        console.log(diets);
+        // console.log(diets);
         res.status(200).json(diets);
     } catch (error) {
         res.status(400).json({error: error.message});
     }    
 }
 
-// await axios.get(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`);
